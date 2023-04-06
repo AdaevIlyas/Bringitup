7cde9428cf0d246ac515aa3353c333c3bb41cb3d
AdaevIlyas <il_adaev@mail.ru>
2023-04-06 11:39:53 +0600

    add show differences

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
+import Differenc from "./modules/differenc";
+
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -37,4 +39,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const player = new VideoPlayer('.showup .play', '.overlay');
   player.init();
+
+  new Differenc('.officerold', '.officernew', '.officer__card-item').init();
 });
